942e9de9effa6eb6cb724e0ab587c5329be1ec17
Benson Wong <wongbsn@gmail.com>
2015-09-01 13:39:06 -0400

    Modified styles and model

--- lib/sections/Landing/template.vue
+++ lib/sections/Landing/template.vue
@@ -8,17 +8,24 @@
   <div class="matrix-container">
     <div v-el="matrix" class="matrix">
       <div class="section">
-        <div class="title">Browser Matrix</div>
+        <div class="title">Desktop</div>
         <div v-el="browsers">
-          <div v-repeat="browser: matrix.browsers" class="row">
+          <div v-repeat="browser: matrix.desktop.browsers" class="row">
             <div class="name">{{browser.name}}</div><div class="version">{{browser.versions}}</div>
           </div>
         </div>
       </div>
       <div class="section">
-        <div class="title">Device Matrix</div>
+        <div class="title">Mobile</div>
+        <div class="subheader">iOS {{matrix.mobile.ios.os}} &nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp; {{matrix.mobile.ios.browser.name}} {{matrix.mobile.ios.browser.versions}}</div>
         <div v-el="devices">
-          <div v-repeat="device: matrix.devices" class="row">
+          <div v-repeat="device: matrix.mobile.ios.devices" class="row">
+            <div class="name">{{device.name}}</div><div class="version">{{device.versions}}</div>
+          </div>
+        </div>
+        <div class="subheader">Android {{matrix.mobile.android.os}}<span>/</span>{{matrix.mobile.android.browser.name}} {{matrix.mobile.android.browser.versions}}</div>
+        <div v-el="devices">
+          <div v-repeat="device: matrix.mobile.android.devices" class="row">
             <div class="name">{{device.name}}</div><div class="version">{{device.versions}}</div>
           </div>
         </div>
