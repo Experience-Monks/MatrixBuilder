16b61255fe3988e3baa3eae195ff9ff7d20f0c8e
Aaron Morris <morrisai@gmail.com>
2015-11-26 17:49:40 -0500

    fixed some bugs, added some rollovers

--- lib/sections/Landing/template.vue
+++ lib/sections/Landing/template.vue
@@ -8,7 +8,7 @@
       <h1 class="title">{{title}}</h1>
        <p class="description">{{{description}}}</p>
        <div class="buttons">
-      <button v-on="click: clear" class="btn btn-clear">Clear</button>
+      <button v-on="click: clear" class="btn btn-clear">Clear<span class="arrow"><img src="assets/arrowDark.png" width="7px" height="14px"/></span></button>
       <button v-el="questionType" v-on="click: onClickQuestionType" class="btn btn-question"></button>
       </div>
       <hr>
@@ -37,8 +37,8 @@
     <div class="matrix-container">
       <div v-el="matrix" class="matrix">
         <div class="section">
-          <h2 class="title">_Desktop</h2>
-          <div v-el="browsers">
+          <h2 class="title" id="_Desktop">_Desktop<span class="arrow"><img src="assets/arrowLight.png" width="14px" height="9px"/></span></h2>
+          <div v-el="browsers" id="_Desktop-content">
             <div v-repeat="matrix.data.desktop.browsers" class="row">
               <div class="name" v-class="error : !$value">{{$key}}</div><div class="version"><span v-repeat="$value">{{$value}}</span></div>
                <hr>
@@ -46,8 +46,8 @@
           </div>
         </div>
         <div class="section">
-          <h2 class="title">_Mobile</h2>
-          <div v-el="ios">
+          <h2 class="title" id="_Mobile">_Mobile<span class="arrow"><img src="assets/arrowLight.png" width="14px" height="9px"/></span></h2>
+          <div v-el="ios" id="_Mobile-content">
             <div class="subheader" v-class="error : !matrix.data.mobile.ios.browser">iOS <span class="val"><span v-repeat="matrix.data.mobile.ios.browser">{{$value}}</span></span>
               <span class="divider">/</span>Safari <span class="val"><span v-repeat="matrix.data.mobile.ios.browser">{{$value}}</span></span>
                <hr>
